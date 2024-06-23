d998f753127af66b4d30b5c850d907911fc05ce8
henrik syvertsson <henrik.syvertsson@gmail.com>
2024-06-22 20:13:30 -0600

    add save/load seed and regenerate simulation

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # ParticleLife
 ```
 todo:
-	- save / load config, interesting premade configs
-	- regenerate w different sim params in-game
 	- cooler graphics
 	- GUI for attraction matrix
 	- add option to run on GPU
--- particle_life.gd
+++ particle_life.gd
@@ -21,32 +21,10 @@ func _ready():
 	$UniverseSphere.scale = Vector3.ONE * $Menu/Particles/UniverseRadiusSpinBox.value * 2
 	
 	var seed = randi()
-	$Menu/Simulation/SeedLabel.set_text("Seed: %d" % seed)
-	seed(seed)
+	$Menu/Simulation/SeedLineEdit.set_text(str(seed))
 
-	for i in range(num_types):
-		attraction_matrix.append([])
-		for j in range(num_types):
-			var val = randf_range(-1, 1)
-			attraction_matrix[i].append(val)
-
-	for i in range(num_particles):
-		var p = Vector3(randf_range(-1,1), randf_range(-1,1), randf_range(-1,1))
-		p = p.normalized()
-		p *= randf_range(0, universe_radius)
-		positions.append(p)
-		velocities.append(Vector3.ZERO)
-		forces.append(Vector3.ZERO)
-		types.append(randi_range(0, num_types - 1))
-	
-	colors = [Color.RED, Color.BLUE, Color.YELLOW]
-
-	multimesh.instance_count = 0
-	multimesh.use_colors = true
-	multimesh.instance_count = num_particles
-	for i in range(num_particles):
-		multimesh.set_instance_color(i, colors[types[i]])
-
+	generate_params(seed)
+	set_multimesh_params()
 
 func _process(delta):
 	
@@ -93,17 +71,45 @@ func _process(delta):
 		multimesh.set_instance_transform(i, t)
 
 
+func generate_params(seed):
+	
+	seed(seed)
+	
+	attraction_matrix = []
+	for i in range(num_types):
+		attraction_matrix.append([])
+		for j in range(num_types):
+			var val = randf_range(-1, 1)
+			attraction_matrix[i].append(val)
+
+	positions = []
+	velocities = []
+	forces = []
+	types = []
+	colors = []
+	for i in range(num_particles):
+		var p = Vector3(randf_range(-1,1), randf_range(-1,1), randf_range(-1,1))
+		p = p.normalized()
+		p *= randf_range(0, universe_radius)
+		positions.append(p)
+		velocities.append(Vector3.ZERO)
+		forces.append(Vector3.ZERO)
+		types.append(randi_range(0, num_types - 1))
+	
+	colors = [Color.RED, Color.BLUE, Color.YELLOW]
+
+
+func set_multimesh_params():
+	multimesh.instance_count = 0
+	multimesh.use_colors = true
+	multimesh.instance_count = num_particles
+	for i in range(num_particles):
+		multimesh.set_instance_color(i, colors[types[i]])
+
+
 func _on_universe_radius_spin_box_value_changed(value):
 	universe_radius = value
 	$UniverseSphere.scale = Vector3.ONE * value * 2
-
-
-func _on_num_particles_spin_box_value_changed(value):
-	num_particles = value
-
-
-func _on_num_types_spin_box_value_changed(value):
-	num_types = value
 
 
 func _on_attraction_radius_spin_box_value_changed(value):
@@ -124,3 +130,12 @@ func _on_max_speed_spin_box_value_changed(value):
 
 func _on_wrap_universe_check_box_toggled(toggled_on):
 	wrap_universe = toggled_on
+
+
+func _on_update_button_pressed():
+	num_particles = $Menu/Simulation/NumParticlesSpinBox.value
+	num_types = $Menu/Simulation/NumTypesSpinBox.value
+	var seed = int($Menu/Simulation/SeedLineEdit.text)
+	generate_params(seed)
+	set_multimesh_params()
+
